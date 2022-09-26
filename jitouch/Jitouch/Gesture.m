d6551ef612bef48743ab8b08aa7a5500cffc8109
Aaron Kollasch <aaron@kollasch.dev>
2022-09-25 23:53:14 -0400

    Remove idf from gestureTrackpadFourFingerTap
    idf is not used in the trackpad tap functions

--- jitouch/Jitouch/Gesture.m
+++ jitouch/Jitouch/Gesture.m
@@ -1416,7 +1416,6 @@ static void gestureTrackpadFourFingerTap(const Finger *data, int nFingers, doubl
     static double sttime = -1;
     static int step = 0;
     static double fing[4][2];
-    static int idf[4];
     if (nFingers > 4)
         step = 2;
     else if (step == 0 && nFingers == 4) {
@@ -1428,22 +1427,6 @@ static void gestureTrackpadFourFingerTap(const Finger *data, int nFingers, doubl
                 fing[i][0] = data[i].px;
                 fing[i][1] = data[i].py;
             }
-            // sort idf according to data[i].px + data[i].py
-            for (int i = 0; i < 4; i++)
-                idf[i] = i;
-            int i = 1, j = 1, swapvar = 0;
-            while (i < 4) {
-                j = i;
-                while (j > 0 && data[j-1].px + data[j-1].py > data[j].px + data[j].py) {
-                    swapvar = idf[j-1];
-                    idf[j-1] = idf[j];
-                    idf[j] = swapvar;
-                    j--;
-                }
-                i++;
-            }
-            for (int i = 0; i < 4; i++)
-                idf[i] = data[idf[i]].identifier;
         }
     } else if (step == 1) {
         if (nFingers <= 1) {
